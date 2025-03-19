0adb58a7fa2f65d6f9044e03b2bdbe2cba8270d0
RAUL7856 <119389914+RAUL7856@users.noreply.github.com>
2025-03-19 09:08:30 +0530

    Update DemoApplication.java
    adding hello world on /

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@RequestMapping("/")
+	String sayHello(){
+		return "Hello World";
+	} 
 
 }
